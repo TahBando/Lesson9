e1bc1a5a0591acb84c4a635ded7bc52fd2c3940a
TahBando <43757539+TahBando@users.noreply.github.com>
2018-12-17 12:35:35 -0500

    Add files via upload

---
+++ Lesson 9/problem2.py
@@ -0,0 +1,10 @@
+print('-'*60)
+print('Locker Bot:')
+print()
+password = input('What is the password? ')
+if password == 'Open Sesame':
+	print('Password is valid!')
+	print()
+else:
+	print('YO MY GUY GET OUTTA HERE')
+	print('-'*60)
---
+++ Lesson 9/problem3.py
@@ -0,0 +1,10 @@
+print('-'*60)
+print('Alien Greeter Bot:')
+print()
+password = input('What planet are you from? ')
+if password == 'Mars':
+	print('Welcome to Earth, please dont kill us!')
+	print()
+else:
+	print('Yo my guy stop playing')
+	print('-'*60)
